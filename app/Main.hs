18cd804252cdcbb521468ddc1611b41268a68ba9
mechpig <7295856+mech-pig@users.noreply.github.com>
2023-03-08 08:39:24 +0100

    feat(ch05): mts exercises

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,8 @@ module Main where
 import qualified Ch01.Hangman
 import qualified Ch01.Vocabulary
 import qualified Ch03.CsvMasterChallenge.Main
+import qualified Ch05.Protected
+import qualified Ch05.StructLog
 import System.Environment (getArgs)
 
 main :: IO ()
@@ -13,4 +15,8 @@ main = do
     ["vocabulary", filePath] -> Ch01.Vocabulary.main filePath
     ["typeclassopedia"] -> putStrLn "tbd"
     ["csv-master-challenge", strategy, filePath] -> Ch03.CsvMasterChallenge.Main.main strategy filePath
+    ["mts", "protected"] -> Ch05.Protected.main Nothing
+    ["mts", "protected", password] -> Ch05.Protected.main $ Just password
+    ["mts", "structlog", "ts"] -> Ch05.StructLog.main True
+    ["mts", "structlog"] -> Ch05.StructLog.main False
     _ -> putStrLn "invalid args"
